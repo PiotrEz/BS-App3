37895163804a5d7adf7cf88745a2f90d85fecc3f
PiotrEz <panpiotruzieblo@gmail.com>
2022-07-18 18:50:17 +0200

    1.1 - Dodane argumenty uruchamiania

--- main.py
+++ main.py
@@ -1,20 +1,31 @@
 from flask import Flask
 from flask import jsonify
 import redis
+import argparse
 
 
-IP_ADDRES = "192.168.0.13"
-PORT_REDIS = 6379
+parser = argparse.ArgumentParser()
+parser.add_argument("i", help="Database IP address")
+parser.add_argument("-pr", "--port_redis", default="6379", help="Redis Database Port")
+parser.add_argument("-p", "--port", default="2500", help="Set application port")
+args = vars(parser.parse_args())
+
+
+REDIS_IP = args["i"]
+PORT_REDIS = args["port_redis"]
+APP_PORT = args["port"]
+
 
 app = Flask(__name__)
+r = redis.Redis(host=REDIS_IP, port=PORT_REDIS, db=0)
 
 
 @app.route('/size', methods=['GET'])
 def test_GET():
-    return jsonify(result=connection(IP_ADDRES,PORT_REDIS))
+    return jsonify(result=connection(REDIS_IP,PORT_REDIS))
+
 
 def connection(IP_ADDRES,PORT_REDIS):
-    r = redis.Redis(host=IP_ADDRES, port=PORT_REDIS,db=0)
     return r.dbsize()
 
-app.run(port=2500)
+app.run(port=APP_PORT)
